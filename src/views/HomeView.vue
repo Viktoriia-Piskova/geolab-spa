d7f299e2be1be03e301c58bf3d2a40618e78fd16
Viktoriia-Piskova <viktoriapiskova@gmail.com>
2023-01-03 18:35:27 +0200

    Initial commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="wrapper">
+    <h1>Posts list</h1>
+    <h2>filter by post title</h2>
+    <p>Title + text + comments count</p>
+    <a href="#">Pagination</a>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+//import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "HomeView",
-  components: {
-    HelloWorld,
-  },
 };
 </script>
---
+++ src/views/MapView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="wrapper">
+    <h1>This is interactive map</h1>
+    <div class="map-wrapper"></div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.wrapper {
+  background-color: red;
+  .map-wrapper {
+    height: 30px;
+    background-color: blue;
+  }
+}
+</style>
